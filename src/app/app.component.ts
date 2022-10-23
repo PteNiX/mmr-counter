3b0e4f7de6b5d1ea8d539fa774ec6ac0b1e24faf
Aliaksandr Ptsitsa <ptenix@gmail.com>
2022-10-23 17:44:35 +0300

    feat: change border line style in container

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -41,8 +41,26 @@ export class AppComponent {
     
   }
 
-    
-   
+
+
+   levenshtein(a: string, b: string): number {
+    const m: number = a.length,
+      n: number = b.length;
+    let t: number[] = [...Array(n + 1).keys()],
+      u: number[] = [];
+    for (let i: number = 0; i < m; i++) {
+      u = [i + 1];
+      for (let j: number = 0; j < n; j++) {
+        u[j + 1] = a[i] === b[j] ? t[j] : Math.min(t[j], t[j + 1], u[j]) + 1;
+      }
+      t = u;
+    }
+    return u[n];
+  }
+
+  ngAfterViewInit() {
+  console.log(this.levenshtein('pten', 'pt3n'));
+  }
 
   async showCurrentMmr () {
     
