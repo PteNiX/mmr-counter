37a292b15d524e02d658f1b8c04131887fc4d9c8
Aliaksandr Ptsitsa <ptenix@gmail.com>
2026-06-21 11:46:44 +0300

    feat: add season 26

--- src/app/main/main.component.ts
+++ src/app/main/main.component.ts
@@ -18,10 +18,10 @@ export class MainComponent implements OnInit {
   title = 'mmr';
   mmrForm: any = {
 
-    season:'24',
+    season:'25',
     race:'4',
     mode:'1',
-    tag:'KAHO#31819',
+    tag:'LeMei#2161',
        
   }
 
@@ -223,7 +223,7 @@ async urlFunction(): Promise<void> {
   else{ 
     this.fullArray=[];
   
-  for(let i=2;i<25; i++) {
+  for(let i=2;i<26; i++) {
 
     this.mmrForm.season=i;
     let preUrlMax = `https://website-backend.w3champions.com/api/matches/search?playerId=${this.mmrForm.tag.trim().replace('#', '%23')}&gateway=20&offset=0&pageSize=100&season=${this.mmrForm.season}&gamemode=${this.mmrForm.mode}`
